39523460f04c0f0c31143ed3f805670a5d9af26b
pinkyrikky <georgechizzy29@gmail.com>
2023-05-11 22:49:44 +0100

    initial setup

---
+++ src/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+// import reportWebVitals from './reportWebVitals';
+
+import './assets/sass/main.scss';
+
+const container = document.getElementById('root')!;
+const root = createRoot(container);
+root.render(
+  <React.StrictMode>
+   <h1>hello</h1>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
